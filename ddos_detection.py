dfe18e3e76612d3f799380540034162e814aabcb
MalinescuIonut <malinescuionut@yahoo.com>
2024-11-04 13:27:42 +0200

    Commit 1

--- ddos_detection.py
+++ ddos_detection.py
@@ -1,6 +1,9 @@
-import kagglehub
+# Import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Download latest version
-path = kagglehub.dataset_download("dhoogla/cicddos2019")
-
-print("Path to dataset files:", path)
+#aici iti alegi calea necesara in functie de ce ai copiat
+syn_train_TCP=pd.read_parquet("Syn-training.parquet", engine='pyarrow')
+#citesti si fisierul de testare.Iti recomand cel putin RandomForests sa folosesti, are rezultate bune
